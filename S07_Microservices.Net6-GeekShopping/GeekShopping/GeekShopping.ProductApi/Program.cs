932c6e37739b546c8f1bdc6c454a490b6e74bcdd
Wilian Amorim Araújo <wilian.a.araujo@hotmail.com>
2022-06-14 21:32:23 -0300

    Implementação da entidade de banco. Criado migration. Instanciada a conexão com base de produtos.

--- S07_Microservices.Net6-GeekShopping/GeekShopping/GeekShopping.ProductApi/Program.cs
+++ S07_Microservices.Net6-GeekShopping/GeekShopping/GeekShopping.ProductApi/Program.cs
@@ -1,3 +1,6 @@
+using GeekShopping.ProductApi.Model.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<MySqlContext>(options =>
+{
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"), new MySqlServerVersion(new Version(8, 0, 6)));
+});
 
 var app = builder.Build();
 
